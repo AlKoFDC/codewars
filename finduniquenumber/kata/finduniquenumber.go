6d7a52d96b31d6dab3966e58d2a1f3a9460765fd
Alexander Kogan <alexander.kogan@freiheit.com>
2017-11-16 10:01:36 +0100

    Kata/find unique number (#1)
    * simple approach
    
    * format changes
    
    * length fix thanks to WS
    
    * move each kata to own package
    
    * small linter fix

---
+++ finduniquenumber/kata/finduniquenumber.go
@@ -0,0 +1,39 @@
+package kata
+
+func FindUniq(arr []float32) float32 {
+	const (
+		firstValue = iota
+		secondValue
+		thirdValue
+	)
+
+	if len(arr) < 3 {
+		panic("Instructions: It’s guaranteed that array contains more than 3 numbers.")
+	}
+
+	// Cover trivial case of 3 values where 1st or 2nd are different.
+	testValue := arr[firstValue]
+	if result, ok := ifFirstAndSecondDifferent(testValue, arr[secondValue], arr[thirdValue]); ok {
+		return result
+	}
+	length := len(arr)
+	for idx := thirdValue; length > idx; idx++ {
+		if value := arr[idx]; testValue != value {
+			return value
+		}
+	}
+	panic("no different value found")
+}
+
+func ifFirstAndSecondDifferent(first, second, third float32) (float32, bool) {
+	if first != second {
+		if first == third {
+			return second, true
+		}
+		return first, true
+	}
+	if third != first {
+		return third, true
+	}
+	return first, false
+}
---
+++ finduniquenumber/kata/finduniquenumber_test.go
@@ -0,0 +1,56 @@
+package kata_test
+
+import (
+	"testing"
+
+	. "github.com/AlKoFDC/codewars/finduniquenumber/kata"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("FindUniq", func() {
+	It("should work for some basic cases", func() {
+		Expect(FindUniq([]float32{1.0, 1.0, 1.0, 2.0, 1.0, 1.0})).To(Equal(float32(2)))
+		Expect(FindUniq([]float32{0, 0, 0.55, 0, 0})).To(Equal(float32(0.55)))
+	})
+})
+
+func TestFindUniq(t *testing.T) {
+	for desc, testIO := range map[string]struct {
+		in  []float32
+		out float32
+	}{
+		"2":      {in: []float32{1.0, 1.0, 1.0, 2.0, 1.0, 1.0}, out: float32(2)},
+		"0.55":   {in: []float32{0, 0, 0.55, 0, 0}, out: float32(0.55)},
+		"first":  {in: []float32{1, 2, 2}, out: float32(1)},
+		"second": {in: []float32{2, 1, 2}, out: float32(1)},
+		"third":  {in: []float32{2, 2, 1}, out: float32(1)},
+	} {
+		t.Run(desc, func(t *testing.T) {
+			got := FindUniq(testIO.in)
+			if got != testIO.out {
+				t.Fatal("in", testIO.in, "expect", testIO.out, "got", got)
+			}
+		})
+	}
+}
+
+func BenchmarkFindUniq(b *testing.B) {
+	const (
+		equal     float32 = 0
+		different float32 = 2
+	)
+	var in []float32
+
+	in = append(in, equal)
+	in = append(in, equal)
+	for n := 0; n < b.N; n++ {
+		in = append(in, equal)
+	}
+	in = append(in, different)
+
+	got := FindUniq(in)
+	if got != different {
+		b.Fatal("in", in, "expect", different, "got", got)
+	}
+}
